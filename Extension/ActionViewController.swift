7fca0ee717ab703f2361bda55392168c245e6539
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-11-27 01:31:59 -0300

    feat: add adjustForKeyboard

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -22,6 +22,10 @@ class ActionViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
         
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
                 if let itemProvider = inputItem.attachments?.first {
                     itemProvider.loadItem(forTypeIdentifier: UTType.propertyList.identifier as String) { [weak self] (dict, error) in
@@ -47,14 +51,40 @@ class ActionViewController: UIViewController {
         let item = NSExtensionItem()
         // set argument from script.text
         let argument: NSDictionary = ["customJavaScript": script.text ?? ""]
-        // insert it in the fnalize dictionary
+        // insert it in the finalize dictionary
         let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
         
+        // Place that NSItemProvider into our NSExtensionItem as its attachments.
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: UTType.propertyList.identifier as String)
             item.attachments = [customJavaScript]
         
         // return items to previous window
         extensionContext?.completeRequest(returningItems: [item])
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        // as NSValue because we can't get cgRectValue directly
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        // gets the size of the keyboard
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        // corrected size of the keyboard if it's rotated
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            // if keyboard is hidding show script from edge to edge
+            script.contentInset = .zero
+        } else {
+            // if its showing resize to be above the keyboard
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        // adjust insets of the scroll indicator to be the same as the content
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+        
     }
 
 }
